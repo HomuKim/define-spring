6938f10021acae729248684731e6680bf916c711
HomuKim <sofood9313@naver.com>
2024-12-27 08:40:37 +0900

    배포용 시작

---
+++ src/test/java/com/example/define/DefineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.define;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
